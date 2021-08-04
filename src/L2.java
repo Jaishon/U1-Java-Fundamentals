10e52f5fe3772564bea51d420e74dccc59c3b9a1
bostijai001 <Jordanbostick2003$>
2021-08-04 12:15:32 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,23 @@
 public class L2 {
 
+    //COMMENTS
+
+
     //add the main method header
+    public static void main(String[] args) {
+     /*String firstname = "George";
+    int age = 16;
+    System.out.println(firstName.toString());
+    System.out.println(age);*/
+
+        String customerName = null;
+        double balance = 1345678.96;
+        int accountID = 156234789;
+
+
+        System.out.println(customerName);
+        System.out.println(balance);
+        System.out.println(accountID);
+
+    }
 }
